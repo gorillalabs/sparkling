2b245f47e110f4191cd394e32a7933ffc991152f
Alex Petrov <alexp@coffeenco.de>
2013-09-17 00:07:06 +0200

    Fix problem with TOC and nested tags

--- assets/javascripts/toc.js
+++ assets/javascripts/toc.js
@@ -1,22 +1,58 @@
 $(document).ready(function() {
   var $toc = $('#toc');
 
-  function format (title) {
-    var $title = $(title),
-        txt = $title.text(),
-        id = $title.attr('id');
-    return "<li> <a href=#" + id + ">" + txt + "</a></li>";
+  function format (item) {
+    if (item.children && item.children.length > 0) {
+      return "<li> <a href=#" + item.id + ">" + item.title + "</a><ul class='nav'>"
+        + item.children.map(format).join('')
+        + "</ul></li>";
+    } else {
+      return "<li> <a href=#" + item.id + ">" + item.title + "</a></li>";
+    }
   }
   // return;
 
   if($toc.length) {
     var $h3s = $('.container .col-md-9 :header');
-    var titles = $h3s.map(function () {
-      return format(this);
-    }).get().join('');
+
+    var tocTree = [];
+    var lastRoot;
+
+    $h3s.each(function(i, el) {
+      var $el = $(el);
+      var id = $el.attr('id');
+      var title = $el.text();
+      var depth = parseInt($el.prop("tagName")[1]);
+
+      if(depth > 3)
+        return;
+
+      if (lastRoot && depth > lastRoot.depth) {
+        lastRoot.children.push({id: id, title: title });
+      } else {
+        lastRoot = {depth: depth,
+                    title: title,
+                    id: id,
+                    children: []};
+        tocTree.push(lastRoot);
+      }
+    });
+
+    var titles = tocTree.map(format).join('');
+
     $toc.html(titles);
   }
 
   $("#toc").affix();
+
+  $('#side-navigation').on('activate.bs.scrollspy', function (e) {
+    var parent = $(e.target).parent().parent()[0];
+
+
+    if (parent.tagName == "LI") {
+      $(parent).addClass("active")
+    }
+
+  })
 
 });
