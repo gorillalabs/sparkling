e6fb229bced85e97858496036946a100615afae6
Alex Petrov <alexp@coffeenco.de>
2013-10-17 13:42:59 +0200

    Fix layout bug

--- assets/javascripts/toc.js
+++ assets/javascripts/toc.js
@@ -43,16 +43,14 @@ $(document).ready(function() {
     $toc.html(titles);
   }
 
-  $("#toc").affix();
+  $("#toc").parent().affix();
 
   $('#side-navigation').on('activate.bs.scrollspy', function (e) {
     var parent = $(e.target).parent().parent()[0];
-
-
     if (parent.tagName == "LI") {
-      $(parent).addClass("active")
+      $(parent).addClass("active");
     }
 
-  })
+  });
 
 });
