f4298b88d813b0a8448f71f843a12519bc3b6b95
Christian Betz <christian.betz@performance-media.de>
2014-12-10 10:59:15 +0100

    started working on release 0.6.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chrisbetz/flambo "0.6.2"
+(defproject chrisbetz/flambo "0.6.3-SNAPSHOT"
             :description "A Clojure Library for Apache Spark"
             :url "https://github.com/chrisbetz/flambo"
             :license {:name "Eclipse Public License"
