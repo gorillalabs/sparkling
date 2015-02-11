bbf0cc4ccb9d1f56747d80902e370b4d6adad766
Christian Betz <christian.betz@performance-media.de>
2015-02-11 10:24:08 +0100

    move to Spark 1.2.1, cleaned up project.clj: Now we test with Spark 1.1.0 and Spark 1.2.1 (which has some problems in my production setting, so I'm not using it currently), Hadoop 2.6.0, Avro 1.7.7

--- project.clj
+++ project.clj
@@ -11,82 +11,63 @@
                             :exclusions [com.twitter/chill-java]]
                            [com.twitter/chill_2.10 "0.5.2"
                             :exclusions [org.scala-lang/scala-library]]
-
-                           ;; [AVRO Feature] This adds support for reading avro files
-                           [com.damballa/parkour "0.6.1" :exclusions [org.codehaus.jackson/jackson-mapper-asl org.codehaus.jackson/jackson-core-asl]]
-                           [com.damballa/abracad "0.4.11" :exclusions [org.apache.avro/avro]]
-                           ;; [/AVRO Feature]
                            ]
 
-            :aliases {"all" ["with-profile" "dev,spark-1.1.0,hadoop-2.6.0:dev,spark-1.2.0,hadoop-2.6.0"]
+            :aliases {"all" ["with-profile" "dev,spark-1.1.0,hadoop-2.6.0,avro-1.7.7-hadoop2:dev,spark-1.2.1,hadoop-2.6.0,avro-1.7.7-hadoop2"]
                       }
-            :profiles {:dev         {:dependencies   [[midje "1.6.3" :exclusions [joda-time commons-codec]]
-                                                      [commons-codec "1.10"]
-                                                      [criterium "0.4.3"]]
-                                     :plugins        [[lein-marginalia "0.8.0"]
-                                                      [lein-ancient "0.5.4"]
-                                                      [codox "0.8.9"]
-                                                      [lein-release "1.0.5"]
-                                                      [lein-pprint "1.1.1"]]
-                                     :resource-paths ["data"]
-                                     ;; so gen-class stuff works in the repl
-                                     :aot            [sparkling.api
-                                                      sparkling.function
-                                                      sparkling.scalaInterop]}
-                       :jenkins     {:plugins [[lein-test-out "0.3.1"]]
-                                     }
-                       :default [:base :system :user :spark-1.2.0 :hadoop-2.6.0 :provided :dev]
-                       :spark-1.1.0 {:dependencies
-                                     [[org.apache.spark/spark-core_2.10 "1.1.0"  :exclusions [commons-io com.thoughtworks.paranamer/paranamer]]
-                                      #_[org.apache.spark/spark-streaming_2.10 "1.1.0" :exclusions [com.thoughtworks.paranamer/paranamer com.fasterxml.jackson.core/jackson-databind]]
-                                      #_[org.apache.spark/spark-streaming-kafka_2.10 "1.1.0" :exclusions [com.thoughtworks.paranamer/paranamer com.fasterxml.jackson.core/jackson-databind]]
-                                      #_[org.apache.spark/spark-sql_2.10 "1.1.0" :exclusions [com.thoughtworks.paranamer/paranamer org.scala-lang/scala-compiler com.fasterxml.jackson.core/jackson-databind]]
+            :profiles {:dev                {:dependencies   [[midje "1.6.3" :exclusions [joda-time commons-codec]]
+                                                             [commons-codec "1.10"]
+                                                             [criterium "0.4.3"]]
+                                            :plugins        [[lein-marginalia "0.8.0"]
+                                                             [lein-ancient "0.5.4"]
+                                                             [codox "0.8.9"]
+                                                             [lein-release "1.0.5"]
+                                                             [lein-pprint "1.1.1"]]
+                                            :resource-paths ["data"]
+                                            ;; so gen-class stuff works in the repl
+                                            :aot            [sparkling.api
+                                                             sparkling.function
+                                                             sparkling.scalaInterop]}
+                       :jenkins            {:plugins [[lein-test-out "0.3.1"]]
+                                            }
+                       :default            [:base :system :user :spark-1.2.1 :hadoop-2.6.0 :avro-1.7.7-hadoop2 :provided :dev]
+                       :avro-1.7.7-hadoop2 {:dependencies [[org.apache.avro/avro "1.7.7" :exclusions [org.codehaus.jackson/jackson-mapper-asl org.codehaus.jackson/jackson-core-asl]]
+                                                           [org.apache.avro/avro-mapred "1.7.7" :classifier "hadoop2" :exclusions [io.netty/netty commons-lang com.thoughtworks.paranamer/paranamer org.slf4j/slf4j-log4j12 org.mortbay.jetty/servlet-api org.codehaus.jackson/jackson-mapper-asl org.codehaus.jackson/jackson-core-asl]]
+                                                           [com.damballa/parkour "0.6.1" :exclusions [org.codehaus.jackson/jackson-mapper-asl org.codehaus.jackson/jackson-core-asl]]
+                                                           [com.damballa/abracad "0.4.11" :exclusions [org.apache.avro/avro]]]}
+                       :spark-1.1.0        {:dependencies
+                                            [[org.apache.spark/spark-core_2.10 "1.1.0" :exclusions [commons-io com.thoughtworks.paranamer/paranamer]]
+                                             #_[org.apache.spark/spark-streaming_2.10 "1.1.0" :exclusions [com.thoughtworks.paranamer/paranamer com.fasterxml.jackson.core/jackson-databind]]
+                                             #_[org.apache.spark/spark-streaming-kafka_2.10 "1.1.0" :exclusions [com.thoughtworks.paranamer/paranamer com.fasterxml.jackson.core/jackson-databind]]
+                                             #_[org.apache.spark/spark-sql_2.10 "1.1.0" :exclusions [com.thoughtworks.paranamer/paranamer org.scala-lang/scala-compiler com.fasterxml.jackson.core/jackson-databind]]
+                                             ]}
 
-                                      ;; [AVRO Feature] This adds support for reading avro files
-                                      [org.apache.avro/avro "1.7.7" :exclusions [org.codehaus.jackson/jackson-mapper-asl org.codehaus.jackson/jackson-core-asl]]
-                                      [org.apache.avro/avro-mapred "1.7.7" :classifier "hadoop2" :exclusions [io.netty/netty commons-lang com.thoughtworks.paranamer/paranamer org.slf4j/slf4j-log4j12 org.mortbay.jetty/servlet-api org.codehaus.jackson/jackson-mapper-asl org.codehaus.jackson/jackson-core-asl]]
-                                      ;; [/AVRO Feature]
+                       :hadoop-2.6.0       {:dependencies
+                                            [[org.apache.hadoop/hadoop-client "2.6.0" :exclusions [org.apache.curator/curator-recipes io.netty/netty org.apache.curator/curator-framework com.google.guava/guava commons-codec commons-net org.slf4j/slf4j-api org.apache.zookeeper/zookeeper]]
+                                             [org.apache.hadoop/hadoop-hdfs "2.6.0" :exclusions [io.netty/netty com.google.guava/guava]]]}
 
-                                      ]}
+                       :spark-1.2.1        {:dependencies
+                                            [[org.apache.spark/spark-core_2.10 "1.2.1" :exclusions [commons-net commons-io com.thoughtworks.paranamer/paranamer]]
+                                             #_[org.apache.spark/spark-streaming_2.10 "1.2.1" :exclusions [com.thoughtworks.paranamer/paranamer com.fasterxml.jackson.core/jackson-databind]]
+                                             #_[org.apache.spark/spark-streaming-kafka_2.10 "1.2.1" :exclusions [com.thoughtworks.paranamer/paranamer com.fasterxml.jackson.core/jackson-databind]]
+                                             #_[org.apache.spark/spark-sql_2.10 "1.2.1" :exclusions [com.thoughtworks.paranamer/paranamer org.scala-lang/scala-compiler com.fasterxml.jackson.core/jackson-databind]]
+                                             ]}
 
-                       :hadoop-2.6.0 {:dependencies
-                                     [[org.apache.hadoop/hadoop-client "2.6.0" :exclusions [org.apache.curator/curator-recipes io.netty/netty org.apache.curator/curator-framework com.google.guava/guava commons-codec commons-net org.slf4j/slf4j-api org.apache.zookeeper/zookeeper]]
-                                      [org.apache.hadoop/hadoop-hdfs "2.6.0" :exclusions [io.netty/netty com.google.guava/guava]]]}
-
-                       :spark-1.2.0 {:dependencies
-                                     [[org.apache.spark/spark-core_2.10 "1.2.0" :exclusions [commons-net commons-io  com.thoughtworks.paranamer/paranamer]]
-                                      #_[org.apache.spark/spark-streaming_2.10 "1.2.0" :exclusions [com.thoughtworks.paranamer/paranamer com.fasterxml.jackson.core/jackson-databind]]
-                                      #_[org.apache.spark/spark-streaming-kafka_2.10 "1.2.0" :exclusions [com.thoughtworks.paranamer/paranamer com.fasterxml.jackson.core/jackson-databind]]
-                                      #_[org.apache.spark/spark-sql_2.10 "1.2.0" :exclusions [com.thoughtworks.paranamer/paranamer org.scala-lang/scala-compiler com.fasterxml.jackson.core/jackson-databind]]
-
-                                      ;; [AVRO Feature] This adds support for reading avro files
-                                      [org.apache.avro/avro "1.7.7" :exclusions [org.codehaus.jackson/jackson-mapper-asl]]
-                                      [org.apache.avro/avro-mapred "1.7.7" :classifier "hadoop2" :exclusions [io.netty/netty commons-lang com.thoughtworks.paranamer/paranamer org.slf4j/slf4j-log4j12 org.mortbay.jetty/servlet-api org.codehaus.jackson/jackson-mapper-asl]]
-                                      ;; [/AVRO Feature]
-
-                                      ]
-                                     :checksum :warn ;https://issues.apache.org/jira/browse/SPARK-5308
-                                     }
-
-
-                       :provided    {:dependencies
-                                     [
-                                      ]}
-                       :test        {:resource-paths ["dev-resources" "data"]
-                                     :aot            [sparkling.api
-                                                      sparkling.function
-                                                      sparkling.scalaInterop
-                                                      sparkling.destructuring
-                                                      sparkling.debug
-                                                      sparkling.rdd.hadoopAvro
-                                                      sparkling.rdd.jdbc
-                                                      sparkling.api-test
-                                                      sparkling.function-test
-                                                      sparkling.conf-test
-                                                      sparkling.rdd.hadoopAvro-test
-                                                      sparkling.rdd.jdbc-test
-                                                      ]}
-                       :uberjar     {:aot :all}
+                       :test               {:resource-paths ["dev-resources" "data"]
+                                            :aot            [sparkling.api
+                                                             sparkling.function
+                                                             sparkling.scalaInterop
+                                                             sparkling.destructuring
+                                                             sparkling.debug
+                                                             sparkling.rdd.hadoopAvro
+                                                             sparkling.rdd.jdbc
+                                                             sparkling.api-test
+                                                             sparkling.function-test
+                                                             sparkling.conf-test
+                                                             sparkling.rdd.hadoopAvro-test
+                                                             sparkling.rdd.jdbc-test
+                                                             ]}
+                       :uberjar            {:aot :all}
                        }
             :source-paths ["src/clojure"]
             :java-source-paths ["src/java"]
