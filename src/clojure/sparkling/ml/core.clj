018eb8952a7cfef61fa325c23b8a5cf39386010d
Kiran Karkera <kiran.karkera@gmail.com>
2016-02-12 18:33:24 +0800

    removed gen-class

--- src/clojure/sparkling/ml/core.clj
+++ src/clojure/sparkling/ml/core.clj
@@ -2,8 +2,7 @@
   "This is the entry point to the machine learning functionality in Spark"
   (:import [org.apache.spark.api.java JavaSparkContext]
     [org.apache.spark.ml Pipeline PipelineModel PipelineStage]
-    [org.apache.spark.sql DataFrame SQLContext ])
- (:gen-class))
+    [org.apache.spark.sql DataFrame SQLContext ]))
 
 (defn sql-context
   "Returns an SQL context by wrapping a JavaSparkContext object "
