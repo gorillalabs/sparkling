e5a596d830d8ece5ad1cd48aef644c8346baa6e7
Kiran Karkera <kiran.karkera@gmail.com>
2016-03-04 12:14:26 +0800

    incremented version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/sparkling "1.2.4-SNAPSHOT"
+(defproject gorillalabs/sparkling "1.2.4.1-SNAPSHOT"
             :description "A Clojure Library for Apache Spark"
             :url "https://gorillalabs.github.io/sparkling/"
             :license {:name "Eclipse Public License"
