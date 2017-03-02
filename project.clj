a62f2281373ca9ab9cb3cd163f09faa99b624dbc
Dr. Christian Betz <cb@cypp.hamburg>
2017-03-02 12:29:43 +0100

    lein-release plugin: preparing 2.0.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/sparkling "2.0.0-SNAPSHOT"
+(defproject gorillalabs/sparkling "2.0.0"
             :description "A Clojure Library for Apache Spark"
             :url "https://gorillalabs.github.io/sparkling/"
             :license {:name "Eclipse Public License"
