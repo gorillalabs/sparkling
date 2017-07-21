a422d7c4fc82883bfb823dbd80a0717acc6e1e82
Dr. Christian Betz <cb.betz@gmail.com>
2017-07-21 17:03:16 +0200

    bump version number to next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/sparkling "2.1.2"
+(defproject gorillalabs/sparkling "2.1.3-SNAPSHOT"
   :description "A Clojure Library for Apache Spark"
   :url "https://gorillalabs.github.io/sparkling/"
   :license {:name "Eclipse Public License"
