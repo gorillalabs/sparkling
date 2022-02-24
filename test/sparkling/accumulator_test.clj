3a9a327b1d24671ee6aa33b51edfffd5567dc358
Olivier Bohrer <olivier.bohrer@onthemarket.com>
2022-02-24 16:59:30 +0000

    Bump spark to 3.2.1 with scala 2.12
    Co-authored-by: obohrer <olivier.bohrer@gmail.com>

--- test/sparkling/accumulator_test.clj
+++ test/sparkling/accumulator_test.clj
@@ -1,5 +1,5 @@
 (ns sparkling.accumulator-test
-  (:import [org.apache.spark Accumulator])
+  (:import [org.apache.spark.util AccumulatorV2])
   (:require [clojure.test :refer :all]
             [sparkling.api :as s]
             [sparkling.conf :as conf]
@@ -16,7 +16,7 @@
       c conf
       (testing
         "gives us an Accumulator Var"
-        (is (= Accumulator (class (ac/accumulator c 0)))))
+        (is (instance? org.apache.spark.util.AccumulatorV2 (ac/accumulator c 0))))
 
       (testing
         "returns a value"
@@ -30,6 +30,32 @@
         "foreach accumulates values"
         (let [a (ac/accumulator c 0.0)]
           (do
-            (-> (s/parallelize c [1. 2. 3. 4. 5.])
+            (-> (s/parallelize c [1. 2. 3. 4. 5.5])
                 (s/foreach (fn [x] (.add a x))))
-            (is (= (ac/value a) 15.0))))))))
+            (is (= (ac/value a) 15.5))))))))
+
+(deftest long-accumulator
+  (let [conf (-> (conf/spark-conf)
+                 (conf/master "local[*]")
+                 (conf/app-name "ac-test"))]
+    (s/with-context
+      c conf
+      (testing
+          "gives us an Accumulator Var"
+        (is (instance? org.apache.spark.util.AccumulatorV2 (ac/long-accumulator c 0))))
+
+      (testing
+          "returns a value"
+        (is (= (ac/value (ac/long-accumulator c 0)) 0)))
+
+      (testing
+          "returns a name"
+        (is (= (ac/name (ac/long-accumulator c 0 "n")) "n")))
+
+      (testing
+          "foreach accumulates values"
+        (let [a (ac/long-accumulator c 0)]
+          (do
+            (-> (s/parallelize c [1 2 3 4 5])
+                (s/foreach (fn [x] (.add a x))))
+            (is (= (ac/value a) 15))))))))
